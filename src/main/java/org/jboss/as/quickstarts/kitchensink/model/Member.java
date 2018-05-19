33e60ddd79e4f9a9502599f74a6c10ceb8340140
tan <voduytan96.vn@gmail.com>
2018-05-19 16:58:31 +0700

    fix

--- src/main/java/org/jboss/as/quickstarts/kitchensink/model/Member.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/model/Member.java
@@ -6,20 +6,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
-
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @XmlRootElement
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+//@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class Member implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -27,23 +18,23 @@ public class Member implements Serializable {
 	@GeneratedValue
 	private Long id;
 
-	@NotNull
-	@Size(min = 1, max = 25)
-	@Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
+	//@NotNull
+	// @Size(min = 1, max = 25)
+	//@Pattern(regexp = "[A-Za-z ]*", message = "Chỉ chứa các chữ cái và khoảng trắng")
 	private String name;
 
 	private String birthdate;
 
-	@NotNull
-	@NotEmpty
-	@Email
+	//@NotNull
+	//@NotEmpty
+	//@Email
 	private String email;
 
-	@NotNull
-	@Size(min = 10, max = 12)
-	@Digits(fraction = 0, integer = 12)
+	// @NotNull
+	// @Size(min = 10, max = 12)
+	// @Digits(fraction = 0, integer = 12)
 	@Column(name = "phone_number")
-	private String phoneNumber;
+	private Long phoneNumber;
 
 	private String address;
 
@@ -79,11 +70,11 @@ public class Member implements Serializable {
 		this.email = email;
 	}
 
-	public String getPhoneNumber() {
+	public Long getPhoneNumber() {
 		return phoneNumber;
 	}
 
-	public void setPhoneNumber(String phoneNumber) {
+	public void setPhoneNumber(Long phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 
