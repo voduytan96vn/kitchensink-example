2eeac09cc697cd4ffd6ce8b2344849176383aef7
Vo Duy Tan <voduytan96.vn@gmail.com>
2018-05-19 10:52:24 +0700

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
 -- You can use this file to load seed data into the database using SQL statements
-insert into Member (id, name, email, phone_number) values (1, 'Nguyễn Văn A', 'a.nguyen@gmail.com', '0123456789') 
+insert into Member (id, name, email, phone_number) values (546546486, 'Nguyễn Văn A', 'a.nguyen@gmail.com', '0123456789') 
